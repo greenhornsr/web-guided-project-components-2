edce4f2de1e8b54cd3b33696f903396b63f99965
greenhornsr <srollins11279@gmail.com>
2022-04-28 20:34:13 -0700

    finished out tasks 1-3

--- src/index.js
+++ src/index.js
@@ -1,22 +1,48 @@
 import axios from "axios"  // this is necessary when installed as a package via npm to package.json; if installed using script tag, not needed.
-console.log(axios)
+
+// Other API Test URL & Endpoints
+'https://lambda-times-api.herokuapp.com/friends'
+'https://lambda-times-api.herokuapp.com/friends/1'
+'https://lambda-times-api.herokuapp.com/quotes'
+'https://lambda-times-api.herokuapp.com/cards'
+'https://lambda-times-api.herokuapp.com/breeds'
+'https://dog.ceo/api/breeds/image/random'
+
+const getDogs = () => {
+  const result = axios.get('https://dog.ceo/api/breeds/image/random')
+
+  .then(res => {
+    console.log(res.data.message)
+    console.log(res.data.status)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
+
 // 👉 TASK 1- Test out the following endpoints:
 
 //  https://dog.ceo/api/breeds/image/random
 
 //  * With Firefox and the Network Tab
 //  * With JS using the native fetch [STRETCH]
+// fetch('https://dog.ceo/api/breeds/image/random')
+// .then(res => res.json())
+// .then(data => console.log(data))
 
 
 // 👉 TASK 2- Select the "entry point", the element
 // inside of which we'll inject our dog cards 
-const entryPoint = null
+const entryPoint = document.querySelector('entry')
 
 
 // 👉 TASK 3- `dogCardMaker` takes an object and returns a Dog Card.
 // Use this function to build a Card, and append it to the entry point.
 function dogCardMaker({ imageURL, breed }) {
   // instantiating the elements
+  const dogCard = document.createElement('div')
+  const dogImage = document.createElement('img')
+  const dogHeading = document.createElement('h3')
   /*
     <div class="dog-card">
       <img class="dog-image">
@@ -24,12 +50,20 @@ function dogCardMaker({ imageURL, breed }) {
     </div>
   */
   // set class names, attributes and text
+  dogHeading.textContent = `Breed: ${breed}`
+  dogImage.src = imageURL
+  dogImage.classList.add('dog-image')
+  dogImage.classList.add('dog-card')
 
   // create the hierarchy
-
+  dogCard.appendChild(dogImage)
+  dogCard.appendChild(dogHeading)
   // add some interactivity
-
+  dogCard.addEventListener('click', () => {
+    dogCard.classList.toggle('selected')
+  })
   // never forget to return!
+  return dogCard
 }
 
 
@@ -42,6 +76,7 @@ function dogCardMaker({ imageURL, breed }) {
 //    * ON SUCCESS: use the data to create dogCards and append them to the entry point
 //    * ON FAILURE: log the error to the console
 //    * IN ANY CASE: log "done" to the console
+
 
 
 // 👉 (OPTIONAL) TASK 6- Wrap the fetching operation inside a function `getDogs`
